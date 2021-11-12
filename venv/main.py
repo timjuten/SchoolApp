69adfce9d03330ffdfc65747e5cd7b28be4cce78
mikola-s <suchariev.nv@gmail.com>
2021-11-12 22:08:44 +0200

    main.py refactoring

--- venv/main.py
+++ venv/main.py
@@ -1,20 +1,26 @@
-from data import user_info #імпортуємо дані з словника у файлі data
+from data import user_info  # імпортуємо дані з словника у файлі data
 
-class Users: #створюємо клас Юзер, в якому буде метод перевірки користувача
-    def __init__(self, email, password): #метод ініт спрацювовує під час ініціалізації об'єкта у класі
-        self.email = email #локальний параметр з емейлом
-        self.password = password #локальний параметр з паролем
 
-    def check_user(self): #метод перевірки користувача
+class Users:
+    """створюємо клас Юзер, в якому буде метод перевірки користувача"""
+    
+    def __init__(self, email: str = None, password: str = None) -> None:
+        """
+        метод ініт спрацювовує під час ініціалізації об'єкта у класі
+        """
+        self.email = email  # локальний параметр з емейлом
+        self.password = password  # локальний параметр з паролем
+    
+    def check_user(self):
+        """
+        метод перевірки користувача
+        """
         if self.email == user_info["email"] and self.password == user_info["password"]:
             print(f"Вітаю, ви є {user_info['user_role']}")
         else:
             print("Я не знаю хто ви")
 
-user = Users("timurjuten@gmail.com", "2345") #створюємо об'єкт у класі юзер
-user.check_user() #визиваємо метод перевірки користувача
 
-
-
-
-
+if __name__ == "__main__":
+    user = Users("timurjuten@gmail.com", "2345")  # створюємо об'єкт у класі юзер
+    user.check_user()  # визиваємо метод перевірки користувача
