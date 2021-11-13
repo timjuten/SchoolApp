cf83818a1b13e248405e4a77196479208bbab469
timjuten <86110908+timjuten@users.noreply.github.com>
2021-11-13 08:50:11 +0200

    Add files via upload

---
+++ User/data.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+conn = sqlite3.connect('users.db')
+
+cur = conn.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS users(
+   userid INT PRIMARY KEY,
+   fname TEXT,
+   lname TEXT,
+   role TEXT,
+   email TEXT,
+   password MESSAGE_TEXT );
+""")
+
+
+def get_data():
+    """
+
+    функція, яка повертає дані з бази даних:
+    """
+    cur.execute("SELECT * FROM users;")
+    three_results = cur.fetchmany(10)
+    data = three_results
+    return data
